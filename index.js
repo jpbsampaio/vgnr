d600d1b746ea363e33d4ba43253592075537f3e5
João Paulo <jpbsampaio@hotmail.com>
2023-12-13 01:30:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const input = document.querySelector("input");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  input.addEventListener("change", function() {
+
+    const text = input.value;
+
+    if (text === "tokstok") {
+
+      alert("cyb wfe bwpb!!! rebv wkj cfb p xpwf np xpcty qbjwfssy hkusorp?");
+    }
+  });
+});
